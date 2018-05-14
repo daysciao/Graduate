03f467ce3c781e075aa2fc4b5f4f8f9174590590
daysciao <zc330@outlook.com>
2018-05-14 21:16:14 +0800

    514

--- mininet_topo_8.py
+++ mininet_topo_8.py
@@ -16,14 +16,14 @@ class MyTopo(Topo):
         h2 = self.addHost('h2',**hostConfig)
         
         # add switchs
-        s0 = self.addSwitch(s0)
-        s1 = self.addSwitch(s1)
-        s2 = self.addSwitch(s2)
-        s3 = self.addSwitch(s3)
-        s4 = self.addSwitch(s4)
-        s5 = self.addSwitch(s5)
-        s6 = self.addSwitch(s6)
-        s7 = self.addSwitch(s7)
+        s0 = self.addSwitch('s0')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
+        s6 = self.addSwitch('s6')
+        s7 = self.addSwitch('s7')
             
         # add links
         self.addLink(h1,s2,**linkConfig)
