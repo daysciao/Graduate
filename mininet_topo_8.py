a22374434a79bdbbee4a50956695feee00a6b312
daysciao <zc330@outlook.com>
2018-05-14 21:07:34 +0800

    5-14

--- mininet_topo_8.py
+++ mininet_topo_8.py
@@ -11,24 +11,37 @@ class MyTopo(Topo):
         hostConfig = {'cpu':cpu}
         linkConfig = {'bw':10,'delay':'10ms','loss':0,'max_queue_size':max_queue_size}
         
-        nodes = (0, 1, 2, 3, 4, 5, 6, 7)
-        edges = [(0, 3), (0, 5), (0, 7), (1, 3), (1, 4), 
-                 (2, 3), (2, 4), (2, 5), (3, 4), (3, 6), 
-                 (3, 7), (4, 5), (4, 6), (4, 7), (5, 6)]
-
         # add hosts
         h1 = self.addHost('h1',**hostConfig)
         h2 = self.addHost('h2',**hostConfig)
         
         # add switchs
-        for node in nodes:
-            self.addSwitch(node)
+        s0 = self.addSwitch(s0)
+        s1 = self.addSwitch(s1)
+        s2 = self.addSwitch(s2)
+        s3 = self.addSwitch(s3)
+        s4 = self.addSwitch(s4)
+        s5 = self.addSwitch(s5)
+        s6 = self.addSwitch(s6)
+        s7 = self.addSwitch(s7)
             
         # add links
-        self.addLink(h1,2,**linkConfig)
-        self.addLink(h2,7,**linkConfig)
-        
-        for v,w in edges:
-            self.addLink(v,w,**linkConfig)
+        self.addLink(h1,s2,**linkConfig)
+        self.addLink(h2,s7,**linkConfig)        
+        self.addLink(s0, s3,**linkConfig)
+        self.addLink(s0, s5,**linkConfig)
+        self.addLink(s0, s7,**linkConfig)
+        self.addLink(s1, s3,**linkConfig)
+        self.addLink(s1, s4,**linkConfig)
+        self.addLink(s2, s3,**linkConfig)
+        self.addLink(s2, s4,**linkConfig)
+        self.addLink(s2, s5,**linkConfig)
+        self.addLink(s3, s4,**linkConfig)
+        self.addLink(s3, s6,**linkConfig)
+        self.addLink(s3, s7,**linkConfig)
+        self.addLink(s4, s5,**linkConfig)
+        self.addLink(s4, s6,**linkConfig)
+        self.addLink(s4, s7,**linkConfig)
+        self.addLink(s5, s6,**linkConfig)
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
