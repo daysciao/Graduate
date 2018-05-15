2eec14748798c918df13e489b2c78bf248062a32
daysciao <zc330@outlook.com>
2018-05-15 23:05:30 +0800

    新たなtopo

--- mininet_topo_8.py
+++ mininet_topo_8.py
@@ -1,50 +1,39 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import CPULimitedHost
+from mininet.node import RemoteController,CPULimitedHost
 from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+
 
 class MyTopo(Topo):
 
-    def __init__(self,cpu=1,max_queue_size=None,**params):
+    def __init__(self):
 
         # initilaize topology   
-        Topo.__init__(self,**params)
+        Topo.__init__(self)
         
+        edges = [(0, 3), (0, 4), (0, 7), (1, 3), (1, 4), (1, 5), (1, 6), (2, 3), (2, 5), (3, 4), (3, 5), (3, 6), (3, 7), (5, 6), (5, 7)]
+        s = []
         node_num = 8
-        hostConfig = {'cpu':cpu}
-        linkConfig = {'bw':10,'delay':'10ms','loss':0,'max_queue_size':max_queue_size}
+        
+        hostConfig = {'cpu':1}
+        linkConfig = {'bw':10,'delay':'10ms','loss':0,'max_queue_size':None}
         
         # add hosts
         h1 = self.addHost('h1',**hostConfig)
         h2 = self.addHost('h2',**hostConfig)
         
         # add switchs
-        s0 = self.addSwitch('s0')
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
-        s4 = self.addSwitch('s4')
-        s5 = self.addSwitch('s5')
-        s6 = self.addSwitch('s6')
-        s7 = self.addSwitch('s7')
+        for i in range(node_num):
+            sw = self.addSwitch( 's{}'.format( i + 1 ) )
+            s.append(sw)
             
         # add links
-        self.addLink(h1,s2,**linkConfig)
-        self.addLink(h2,s7,**linkConfig)        
-        self.addLink(s0, s3,**linkConfig)
-        self.addLink(s0, s5,**linkConfig)
-        self.addLink(s0, s7,**linkConfig)
-        self.addLink(s1, s3,**linkConfig)
-        self.addLink(s1, s4,**linkConfig)
-        self.addLink(s2, s3,**linkConfig)
-        self.addLink(s2, s4,**linkConfig)
-        self.addLink(s2, s5,**linkConfig)
-        self.addLink(s3, s4,**linkConfig)
-        self.addLink(s3, s6,**linkConfig)
-        self.addLink(s3, s7,**linkConfig)
-        self.addLink(s4, s5,**linkConfig)
-        self.addLink(s4, s6,**linkConfig)
-        self.addLink(s4, s7,**linkConfig)
-        self.addLink(s5, s6,**linkConfig)
+        
+        self.addLink(h2,s[4],**linkConfig)       
+        
+        for v,w in edges:
+            self.addLink(s[v],s[w],**linkConfig)
+        
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
