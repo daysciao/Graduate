0eada94a1baae76c5df13e36b3c63b20081e0f6c
daysciao <zc330@outlook.com>
2018-05-28 11:54:35 +0800

    更多节点的拓扑

--- mininet_topo_16.py
+++ mininet_topo_16.py
@@ -12,10 +12,12 @@ class MyTopo(Topo):
         # initilaize topology   
         Topo.__init__(self)
         
-        edges = [(0, 2), (0, 3), (1, 2), (2, 3), (2, 4), (2, 5), 
-                 (2, 6), (3, 4), (3, 5), (3, 7), (4, 7), (5, 6)]
+        edges = [[(0, 2), (0, 4), (0, 7), (1, 2), (1, 3), (1, 7), (1, 10), 
+                  (1, 11), (1, 13), (2, 3), (2, 5), (2, 6), (2, 10), (2, 12), 
+                  (2, 13), (3, 8), (3, 4), (4, 5), (4, 15), (5, 6), (5, 8), 
+                  (5, 9), (5, 12), (5, 14), (6, 14), (7, 11), (7, 9), (11, 15)]
         s = []
-        node_num = 8
+        node_num = 16
         
         hostConfig = {'cpu':1}
         
@@ -31,7 +33,8 @@ class MyTopo(Topo):
         # add links
         
         self.addLink(h1,s[0])
-        self.addLink(h2,s[4])
+        self.addLink(h2,s[8])
+
         
         for v,w in edges:
             self.addLink(s[v],s[w])
